a1ee9b1f9b113aa1da94e79a7496be341973eb9d
leejohy <leejohy@naver.com>
2021-04-23 15:56:09 +0900

    session -> cookie change & github erase / re-commit test

--- src/main/java/com/newlecture/web/Calc2.java
+++ src/main/java/com/newlecture/web/Calc2.java
@@ -6,6 +6,7 @@ import java.io.PrintWriter;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -19,6 +20,7 @@ public class Calc2 extends HttpServlet {
 
 		ServletContext application = request.getServletContext();
 		HttpSession session = request.getSession();
+		Cookie[] cookies = request.getCookies();
 		
 		response.setCharacterEncoding("UTF-8");
 		response.setContentType("text/html; charset=UTF-8");
@@ -37,10 +39,26 @@ public class Calc2 extends HttpServlet {
 		if (op.equals("=")) {
 			
 //			int x = (Integer)application.getAttribute("value");
-			int x = (Integer)session.getAttribute("value");
+//			int x = (Integer)session.getAttribute("value");
+			
+			int x = 0;
+			for(Cookie c : cookies) {
+				if(c.getName().equals("value")) {
+					x = Integer.parseInt(c.getValue());
+					break;
+				}
+			}
+			
 			int y = v;
 //			String operator = (String)application.getAttribute("op");
-			String operator = (String)session.getAttribute("op");
+//			String operator = (String)session.getAttribute("op");
+			String operator = "";
+			for(Cookie c : cookies) {
+				if(c.getName().equals("op")) {
+					operator = c.getValue();
+					break;
+				}
+			}
 			
 			int result = 0;
 			
@@ -53,9 +71,14 @@ public class Calc2 extends HttpServlet {
 		} else {
 //			application.setAttribute("value", v);
 //			application.setAttribute("op", op);
-			session.setAttribute("value", v);
-			session.setAttribute("op", op);
+			
+//			session.setAttribute("value", v);
+//			session.setAttribute("op", op);
 
+			Cookie valueCookie = new Cookie("value", String.valueOf(v));
+			Cookie opCookie = new Cookie("op", op);
+			response.addCookie(valueCookie);
+			response.addCookie(opCookie);
 		}
 	}
 }
